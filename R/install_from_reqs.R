2d9445b16d90734b5580cd699d02e59a129de92b
joan-yanqiong <j.b.y.kant@student.tue.nl>
2023-07-24 17:24:36 -0400

    update namespace

--- R/install_from_reqs.R
+++ R/install_from_reqs.R
@@ -2,7 +2,7 @@
 #'
 #' @param req_path path to requirements file
 #' @export
-function(req_path) {
+install_from_reqs <- function(req_path) {
     reqs <- read.csv(req_path, row.names = 1)
     pkgs_to_install <- reqs %>% pull(pkg_incl_version)
     pkgs_to_install <- pkgs_to_install[!is.na(pkgs_to_install)]
