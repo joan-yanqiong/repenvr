b769b821d88713cfd31eb1e29a6d69a3f3b42592
Joan <43519278+joan-yanqiong@users.noreply.github.com>
2023-07-27 10:10:33 -0400

    adapt install from reqs to install bioconductor pkgs separately without version control.

--- R/install_from_reqs.R
+++ R/install_from_reqs.R
@@ -2,14 +2,37 @@
 #'
 #' @param req_path path to requirements file
 #' @export
-#' @importFrom dplyr %>% pull
+#' @importFrom dplyr %>% pull filter
 #' @importFrom pak pkg_install
-install_from_reqs <- function(req_path) {
+install_from_reqs <- function(req_path, libpath) {
     if (!file.exists(req_path)) {
         stop("Invalid path to requirements file.")
     }
     reqs <- read.csv(req_path, row.names = 1)
-    pkgs_to_install <- reqs %>% pull(pkg_incl_version)
-    pkgs_to_install <- pkgs_to_install[!is.na(pkgs_to_install)]
-    pkg_install(pkgs_to_install)
+    if (nrow(reqs) < 1) {
+        stop("No packages to install.")
+    }
+
+    # Check if package is available, only base, cran and bioconductor packages
+    pkgs_base <- data.frame(installed.packages(priority = "base")) %>% pull(Package)
+    pkgs_bioconductor <- available()
+    pkgs_cran <- data.frame(CRAN_package_db()) %>% pull(Package)
+    pkgs_available <- c(pkgs_base, pkgs_bioconductor, pkgs_cran)
+
+    # install packages
+    avail_known <- reqs %>%
+        filter(source %in% c("CRAN", "Base")) %>%
+        pull(pkg_incl_version)
+    avail_unknown <- reqs %>%
+        filter(source %in% c("Github", "Other")) %>%
+        pull(pkg_incl_version)
+    # Install packages from all packages except bioconductor
+    pak::pkg_install(c(avail_known, avail_unknown), lib = libpath)
+
+    # TODO: installing specific bioconductor packages not possible yet with pak
+    # adapt when this changes
+    reqs %>%
+        filter(source == "Bioconductor", Package %in% pkgs_bioconductor) %>%
+        pull(Package) %>%
+        pak::pkg_install(., lib = libpath)
 }
